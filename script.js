764e15310b18ea3427c047c106331335dc4a2878
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-14 01:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // complete the given function
 
-function palindrome(str){
-	let rev = str.split("").reverse().join("");
-	console.log(rev);
+function palindrome(){
+
+	let userInput = document.getElementById("input").value;
+
+    let splitted = userInput.split(" ").join("");
+
+    let stringrev = "";
+
+    for(let i = splitted.length - 1; i > -1 ; i--){
+        stringrev = stringrev + splitted.charAt(i);
+    }
+
+
+
+    if(splitted === stringrev){
+        document.getElementById("ans").innerHTML = "True";
+    }else{
+        document.getElementById("ans").innerHTML = "False";
+    }
 }
-module.exports = palindrome
+
